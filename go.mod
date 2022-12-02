b0ebfcca55818dd030e4421b59f158382b2c92dc
Presnyakov Aleksandr Evgenevich <apresnyakov@mtsbank.ru>
2022-12-02 16:29:09 +0300

    go-bindata-assetfs added

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/presnalex/swagger-ui-route
 
 go 1.14
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/elazarl/go-bindata-assetfs v1.0.0
+	github.com/gorilla/mux v1.7.4
+)
--- swagger.go
+++ swagger.go
@@ -1,9 +1,10 @@
 package swagger
 
 import (
-	"io/fs"
 	"net/http"
+	"os"
 
+	assetfs "github.com/elazarl/go-bindata-assetfs"
 	"github.com/gorilla/mux"
 )
 
@@ -12,24 +13,26 @@ const (
 	path_noslash = "/swagger-ui"
 )
 
-func Register(r *mux.Router, appName string, embededFiles fs.FS) {
+type asset func(string) ([]byte, error)
+type assetdir func(string) ([]string, error)
+type assetinfo func(string) (os.FileInfo, error)
+
+func Register(r *mux.Router, appName string, a asset, d assetdir, i assetinfo) {
 	basePath := "/" + appName
 	r.PathPrefix(
 		basePath + path_slash).Handler(
 		http.StripPrefix(basePath+path_slash,
-			http.FileServer(getFileSystem(embededFiles)),
+			http.FileServer(
+				&assetfs.AssetFS{
+					Asset:     a,
+					AssetDir:  d,
+					AssetInfo: i,
+					Prefix:    "/",
+				},
+			),
 		),
 	)
 	r.HandleFunc(basePath+path_noslash, func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, basePath+path_slash, http.StatusMovedPermanently)
 	})
-}
-
-func getFileSystem(embededFiles fs.FS) http.FileSystem {
-	fsys, err := fs.Sub(embededFiles, "swagger-ui")
-	if err != nil {
-		panic(err)
-	}
-
-	return http.FS(fsys)
 }
